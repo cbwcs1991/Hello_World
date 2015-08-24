359a21bc22c17ddb326635a0b3e28f78f6b463b3
CBWCS <cbwcs@ubuntu.ubuntu-domain>
2015-08-23 18:34:33 -0700

    fking boring

---
+++ recordmax.cpp
@@ -0,0 +1,14 @@
+vector<int> recordmax(vector<int>& nums)
+{
+	vector<int> ret;
+	int n=nums.size();
+	if(!n) return;
+	stack<int> s;
+	for(int i=0;i<n;i++)
+	{
+		if(!s.empty||nums[i]<=s.top())
+		{
+			fk this editer is so hard to use;
+		}
+	}
+}
